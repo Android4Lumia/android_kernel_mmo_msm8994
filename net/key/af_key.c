047c11dd06849dfe6a393bdddff80cfbf9418bde
J0SH1X <aljoshua.hell@gmail.com>
2025-02-20 17:29:40 +0100

    net: fix compilation after selinux merge

--- net/key/af_key.c
+++ net/key/af_key.c
@@ -3245,7 +3245,7 @@ static struct xfrm_policy *pfkey_compile_policy(struct sock *sk, int opt,
 		}
 		if ((*dir = verify_sec_ctx_len(p)))
 			goto out;
-		uctx = pfkey_sadb2xfrm_user_sec_ctx(sec_ctx, GFP_ATOMIC);
+		uctx = pfkey_sadb2xfrm_user_sec_ctx(sec_ctx);
 		*dir = security_xfrm_policy_alloc(&xp->security, uctx, GFP_ATOMIC);
 		kfree(uctx);
 
